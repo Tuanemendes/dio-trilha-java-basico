bd864d6382f2710e140f7f15f6fd2a7edb2571e2
Tuanemendes <tuane.belmiro@gmail.com>
2022-12-12 15:49:45 -0300

    inico do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        
+
     }
 }
